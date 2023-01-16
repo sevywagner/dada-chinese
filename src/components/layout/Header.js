50fd01a530e07d50b5cd6ba3e034dfee42ac947b
Sevy Wagner <sevywagner@Sevys-MBP.attlocal.net>
2023-01-16 01:25:00 -0600

    updated links

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,24 +6,24 @@ const Header = () => {
     <header>
       <p className={styles['header-text']}>Dada Chinese</p>
       <nav>
-        <Link className={styles["nav-item"]} to="/">
+        <Link className={styles["nav-item"]} to="/dada-chinese">
           Home
         </Link>
         <div className={styles.subnav}>
-          <Link className={styles["nav-item"]} to='/book-online'>Book Online</Link>
+          <Link className={styles["nav-item"]} to='/dada-chinese/book-online'>Book Online</Link>
           <div className={styles['subnav-content']}>
-            <Link className={styles["subnav-item"]} to="/plans">
+            <Link className={styles["subnav-item"]} to="/dada-chinese/plans">
               Plans & Pricing
             </Link>
           </div>
         </div>
-        <Link className={styles["nav-item"]} to="/blog">
+        <Link className={styles["nav-item"]} to="/dada-chinese/blog">
           Blog
         </Link>
-        <Link className={styles["nav-item"]} to="/members">
+        <Link className={styles["nav-item"]} to="/dada-chinese/members">
           Members
         </Link>
-        <Link className={styles['sign-in']} to='/sign-in'>Sign in</Link>
+        <Link className={styles['sign-in']} to='/dada-chinese/sign-in'>Sign in</Link>
       </nav>
     </header>
   );
